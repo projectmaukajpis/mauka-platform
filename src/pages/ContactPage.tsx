ead90a939a4af462b1e9f6adebae14d9e03f2f12
projectmaukajpis <projectmaukajpis@gmail.com>
2025-09-10 21:25:44 +0530

    Updated ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -174,7 +174,7 @@ export default function ContactPage() {
                   your application.
                 </p>
                 <a 
-                  href="https://forms.google.com/your-ngo-form-id" 
+                  href="https://forms.gle/FPrgRb171kFwd9v88" 
                   target="_blank" 
                   rel="noopener noreferrer"
                   className="text-blue-600 hover:text-blue-700 font-medium"
